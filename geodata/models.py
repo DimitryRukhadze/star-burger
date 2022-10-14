a00fb69cd2a55672b75f832aa70e982039c203c3
DimitryRukhadze <lokiinflames@gmail.com>
2022-10-14 09:56:03 +0300

    added verbose names to models geodata

--- geodata/models.py
+++ geodata/models.py
@@ -7,6 +7,6 @@ class PlaceGeo(models.Model):
         max_length=200,
         unique=True
     )
-    lon = models.DecimalField(max_digits=16, decimal_places=14)
-    lat = models.DecimalField(max_digits=16, decimal_places=14)
-    last_update = models.DateTimeField(auto_now=True)
+    lon = models.DecimalField(max_digits=16, decimal_places=14, verbose_name='Долгота')
+    lat = models.DecimalField(max_digits=16, decimal_places=14, verbose_name='Широта')
+    last_update = models.DateTimeField(auto_now=True, verbose_name='Последнее обновление')
